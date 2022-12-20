f0a87d3bc0c14e7420920f36a16459ef07a32f16
Rasmus Thomsen <oss@cogitri.dev>
2022-12-20 13:58:51 +0100

    poppler: upgrade qt dependency

--- recipes/poppler/all/conanfile.py
+++ recipes/poppler/all/conanfile.py
@@ -97,7 +97,7 @@ class PopplerConan(ConanFile):
         if self.options.get_safe("with_gobject_introspection"):
             self.requires("gobject-introspection/1.72.0")
         if self.options.with_qt:
-            self.requires("qt/6.3.1")
+            self.requires("qt/6.4.1")
         if self.options.with_openjpeg:
             self.requires("openjpeg/2.5.0")
         if self.options.with_lcms:
