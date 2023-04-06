aa6b920471fcd2306e8d7b0f4020ae17448a3bc1
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-04-06 11:26:32 +0200

    soci: use newest libmysqlclient on all platforms

--- recipes/soci/all/conanfile.py
+++ recipes/soci/all/conanfile.py
@@ -72,10 +72,7 @@ class SociConan(ConanFile):
         if self.options.with_odbc and self.settings.os != "Windows":
             self.requires("odbc/2.3.9")
         if self.options.with_mysql:
-            if self.settings.os == "Windows":
-                self.requires("libmysqlclient/8.0.17")
-            else:
-                self.requires("libmysqlclient/8.0.31")
+            self.requires("libmysqlclient/8.0.31")
         if self.options.with_postgresql:
             self.requires("libpq/14.7")
         if self.options.with_boost:
