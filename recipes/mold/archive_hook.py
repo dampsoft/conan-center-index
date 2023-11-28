4077b587fd75219cd83e76f71f87bc4bbd2dd8b6
Rasmus Thomsen <rasmus.thomsen@dampsoft.de>
2023-11-28 08:51:40 +0100

    mold: update archive_hook to include installing patchelf

--- recipes/mold/archive_hook.py
+++ recipes/mold/archive_hook.py
@@ -1,20 +1,58 @@
 from os import mkdir
+import os
 from pathlib import Path
 from conan.api.conan_api import ConanAPI
 from conans.model.recipe_ref import RecipeReference
 from conans.util.files import rmdir, remove
 from conan.errors import ConanException
+from conan.cli.printers import ConanOutput
 import shutil
 import subprocess
+from conan.cli.printers.graph import print_graph_packages
 
-def patch_rpath(target_rpath: str, bin_path: Path):
-    command = ["patchelf", "--force-rpath", "--set-rpath", target_rpath, bin_path]
+def patch_rpath(patchelf_path: str, target_rpath: str, bin_path: Path):
+    command = [patchelf_path, "--force-rpath", "--set-rpath", target_rpath, bin_path]
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if process.returncode != 0:
         ConanException(f"Error executing command: {process.stderr}")
 
+def install_patchelf(out: ConanOutput, deploy_folder: Path, conan_api: ConanAPI) -> str:
+    out.info("Installing patchelf")
+
+    enabled_remotes = conan_api.remotes.list()
+    graph = conan_api.graph.load_graph_requires(
+        requires=["patchelf/0.13"],
+        tool_requires=None,
+        lockfile=None,
+        profile_build=conan_api.profiles.get_profile(
+            profiles=[conan_api.profiles.get_default_build()]
+        ),
+        profile_host=conan_api.profiles.get_profile(
+            profiles=[conan_api.profiles.get_default_host()]
+        ),
+        remotes=enabled_remotes,
+        update=False,
+    )
+    graph.report_graph_error()
+
+    conan_api.graph.analyze_binaries(graph, remotes=enabled_remotes, build_mode=["never"])
+    print_graph_packages(graph)
+    conan_api.install.install_binaries(deps_graph=graph)
+
+    conan_api.install.install_consumer(
+        deps_graph=graph,
+        output_folder=str(deploy_folder/ 'patchelf'),
+        source_folder=os.getcwd(),
+        deploy=["ds_nexus_deploy"],
+    )
+
+    return deploy_folder / 'patchelf' / 'ds_nexus_deploy' / 'out'  / 'bin' / 'patchelf'
 
 def archive_hook(pkg: RecipeReference, deploy_folder: Path, conan_api: ConanAPI):
+    out = ConanOutput()
+
+    patchelf_path = install_patchelf(out, deploy_folder, conan_api)
+
     rmdir(deploy_folder / 'include')
     remove(deploy_folder / 'bin' / 'xxhsum')
 
@@ -22,8 +60,14 @@ def archive_hook(pkg: RecipeReference, deploy_folder: Path, conan_api: ConanAPI)
     mkdir(deploy_folder / 'lib')
     shutil.move(deploy_folder / 'mold_lib', deploy_folder / 'lib' / 'ds-mold')
 
+    out.info("Patching RPATH in mold binaries")
+
     for x in (deploy_folder / 'lib').glob('*.so'):
-        patch_rpath('$ORIGIN', x)
+        patch_rpath(patchelf_path, '$ORIGIN', x)
 
     for x in (deploy_folder / 'bin').glob('*'):
-        patch_rpath('$ORIGIN/../lib/ds-mold', x)
+        patch_rpath(patchelf_path, '$ORIGIN/../lib/ds-mold', x)
+
+    rmdir(deploy_folder / 'patchelf')
+
+    out.success("Done, will generate archive now!")
