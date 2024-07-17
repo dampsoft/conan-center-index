a0241ae9137081b1c10066ec352679624407eac3
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2024-07-17 14:11:15 +0200

    libpq: fix patch build errors

--- recipes/libpq/all/conanfile.py
+++ recipes/libpq/all/conanfile.py
@@ -151,9 +151,15 @@ class LibpqConan(ConanFile):
                 system_libs.extend(dep.cpp_info.aggregated_components().system_libs)
 
             linked_system_libs = ", ".join(["'{}.lib'".format(lib) for lib in system_libs])
-            replace_in_file(self,os.path.join(self.source_folder, "src", "tools", "msvc", "Project.pm"),
+            
+            if Version(self) < "16":
+                replace_in_file(self,os.path.join(self.source_folder, "src", "tools", "msvc", "Project.pm"),
                                   "libraries             => [],",
                                   "libraries             => [{}],".format(linked_system_libs))
+            else:
+                replace_in_file(self,os.path.join(self.source_folder, "src", "tools", "msvc", "Project.pm"),
+                                  "libraries => [],",
+                                  "libraries => [{}],".format(linked_system_libs))
             runtime = {
                 "MT": "MultiThreaded",
                 "MTd": "MultiThreadedDebug",
