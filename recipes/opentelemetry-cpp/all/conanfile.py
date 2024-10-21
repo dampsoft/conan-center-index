a62db6ef1d3c3847940fa04961dc6bc6d3c85e5c
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2024-10-21 13:01:19 +0200

    opentelemetry-cpp:
    - add v 1.17.0
    - depend on opentelemetry-proto/1.3.2

--- recipes/opentelemetry-cpp/all/conanfile.py
+++ recipes/opentelemetry-cpp/all/conanfile.py
@@ -246,7 +246,7 @@ class OpenTelemetryCppConan(ConanFile):
 
     def build_requirements(self):
         if self.options.with_otlp_grpc or self.options.with_otlp_http:
-            self.tool_requires("opentelemetry-proto/1.3.0")
+            self.tool_requires("opentelemetry-proto/1.3.2")
             self.tool_requires("protobuf/<host_version>")
 
         if self.options.with_otlp_grpc:
