20081563272de4954b38a289807657583ba033ad
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2024-11-15 17:11:54 +0100

    dcmtk: bump deps

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -98,7 +98,7 @@ class DCMTKConan(ConanFile):
         elif self.options.charset_conversion == "icu":
             self.requires("icu/75.1")
         if self.options.with_libxml2:
-            self.requires("libxml2/2.12.4")
+            self.requires("libxml2/2.13.4")
         if self.options.with_zlib:
             self.requires("zlib/[>=1.2.11 <2]")
         if self.options.with_openssl:
