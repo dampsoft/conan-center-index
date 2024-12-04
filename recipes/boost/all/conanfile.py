3bf1dd26df8c7be3c0116b0c4327a6991968054f
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2024-12-04 09:52:55 +0100

    depend on icu/76.1

--- recipes/boost/all/conanfile.py
+++ recipes/boost/all/conanfile.py
@@ -822,7 +822,7 @@ class BoostConan(ConanFile):
             self.requires("libbacktrace/cci.20210118", transitive_headers=True, transitive_libs=True)
 
         if self._with_icu:
-            self.requires("icu/75.1")
+            self.requires("icu/76.1")
         if self._with_iconv:
             self.requires("libiconv/1.17")
 
