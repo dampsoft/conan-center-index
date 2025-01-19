61c794ecc6ac8d5b578128323b496dd1945970a2
Khalid Aljayoussi <programer_21@hotmail.com>
2025-01-19 17:27:14 -0500

    change libxml2 dependency to > 2.12

--- recipes/dcmtk/all/conanfile.py
+++ recipes/dcmtk/all/conanfile.py
@@ -99,7 +99,7 @@ class DCMTKConan(ConanFile):
         elif self.options.charset_conversion == "icu":
             self.requires("icu/76.1")
         if self.options.with_libxml2:
-            self.requires("libxml2/2.13.4")
+            self.requires("libxml2/[>=2.12.5 <3]")
         if self.options.with_zlib:
             self.requires("zlib/[>=1.2.11 <2]")
         if self.options.with_openssl:
