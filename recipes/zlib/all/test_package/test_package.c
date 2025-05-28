7c0e29f949e907b3a375eab6b948e651e1e06eb6
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-05-28 17:10:01 +0200

    zlib: cr for tests

--- recipes/zlib/all/test_package/test_package.c
+++ recipes/zlib/all/test_package/test_package.c
@@ -5,14 +5,17 @@
 
 int zlib_test_vsnprintf(void) {
 #ifdef WITH_TEST_VSNPRINTF
-    uLong flags = zlibCompileFlags();
-
     // 25th bit indicates vsnprinft support
     // 26th bit indicates whether vnsprinft returned void at compile time (i.e. doesn't work correctly)
     // from the zlib.h header:
     // > 25: 0 = *nprintf, 1 = *printf -- 1 means gzprintf() not secure!
     // > 26: 0 = returns value, 1 = void -- 1 means inferred string length returned
-    if (flags >> 25 || flags >> 26) {
+    uLong flags = zlibCompileFlags();
+
+    uLong flag_vsnprintf_support = 1L << 25;
+    uLong flag_vsnprintf_void_ret = 1L << 26;
+
+    if ((flags & flag_vsnprintf_support) == flag_vsnprintf_support || (flags & flag_vsnprintf_void_ret) == flag_vsnprintf_void_ret) {
         printf("ZLIB is not compiled with vnsprinft support\n");
         return EXIT_FAILURE;
     }
