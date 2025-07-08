90308f856d9ccd470d26627812c729f88d6aa626
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-07-08 09:50:55 +0200

    valgrind: fix test

--- recipes/valgrind/all/test_package/conanfile.py
+++ recipes/valgrind/all/test_package/conanfile.py
@@ -1,9 +1,23 @@
+from io import StringIO
+from pathlib import Path
 from conan import ConanFile
+from conan.tools.build import can_run
 
 
 class ValgrindTestConan(ConanFile):
-    def build_requirements(self):
-        self.tool_requires(self.tested_reference_str)
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def test(self):
-        self.run("valgrind --version", env="conanrun")
+        if can_run(self):
+            prefix = self.dependencies["valgrind"].options.prefix
+            bin_path = self.dependencies["valgrind"].package_folder.replace("\\", "/")
+            bin_path = Path(f"{bin_path}/{prefix}/bin/valgrind")
+
+            stdout, stderr = StringIO(), StringIO()
+            self.run(f"{bin_path} --version", env="conanrun", stdout=stdout, stderr=stderr)
+
+            print(stderr.getvalue())
+            print(stdout.getvalue())
