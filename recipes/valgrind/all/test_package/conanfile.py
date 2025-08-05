f64b488d9285cf233cc11618618550f4307cd058
Tim Friedrich Brüggemann <tim.brueggemann@dampsoft.de>
2025-08-05 13:36:16 +0200

    valgrind: fix test env

--- recipes/valgrind/all/test_package/conanfile.py
+++ recipes/valgrind/all/test_package/conanfile.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from pathlib import Path
 from conan import ConanFile
+from conan.tools.env import Environment
 from conan.tools.build import can_run
 
 
@@ -13,11 +14,12 @@ class ValgrindTestConan(ConanFile):
     def test(self):
         if can_run(self):
             prefix = self.dependencies["valgrind"].options.prefix
-            bin_path = self.dependencies["valgrind"].package_folder.replace("\\", "/")
-            bin_path = Path(f"{bin_path}/{prefix}/bin/valgrind")
+            pkg_path = Path(self.dependencies["valgrind"].package_folder.replace("\\", "/"))
+            pkg_path = Path(f"{pkg_path}/{prefix}")
+            bin_path = Path(f"{pkg_path}/bin/valgrind")
 
-            stdout, stderr = StringIO(), StringIO()
-            self.run(f"{bin_path} --version", env="conanrun", stdout=stdout, stderr=stderr)
+            env = Environment()
+            env.define("VALGRIND_LIB", str(Path(f"{pkg_path}/libexec/valgrind")))
 
-            print(stderr.getvalue())
-            print(stdout.getvalue())
+            with env.vars(self).apply():
+                self.run(f"{bin_path} --version", env="conanrun")
